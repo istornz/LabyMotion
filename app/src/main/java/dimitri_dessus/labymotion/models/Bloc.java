b646f2289a74933d9a3eb450bf9ddb8dad9f06a4
Dessus Dimitri <iphoneretro@icloud.com>
2017-04-14 18:40:28 +0200

    [Add] Bloc class

---
+++ app/src/main/java/dimitri_dessus/labymotion/models/Bloc.java
@@ -0,0 +1,29 @@
+package dimitri_dessus.labymotion.models;
+
+import android.graphics.RectF;
+
+/**
+ * Created by Dimitri on 14/04/2017.
+ */
+
+public class Bloc {
+    enum  Type { HOLE, START, END };
+
+    private float SIZE = Ball.RADIUS * 2;
+
+    private Type mType = null;
+    private RectF mRectangle = null;
+
+    public Type getType() {
+        return mType;
+    }
+
+    public RectF getRectangle() {
+        return mRectangle;
+    }
+
+    public Bloc(Type pType, int pX, int pY) {
+        this.mType = pType;
+        this.mRectangle = new RectF(pX * SIZE, pY * SIZE, (pX + 1) * SIZE, (pY + 1) * SIZE);
+    }
+}
