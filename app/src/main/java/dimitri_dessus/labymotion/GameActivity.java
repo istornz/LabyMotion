60003913d5104540d7084b7c8ea647450eb29867
Dessus Dimitri <iphoneretro@icloud.com>
2017-04-14 19:10:40 +0200

    [Add] GameActivity class

--- app/src/main/java/dimitri_dessus/labymotion/GameActivity.java
+++ app/src/main/java/dimitri_dessus/labymotion/GameActivity.java
@@ -1,13 +1,95 @@
 package dimitri_dessus.labymotion;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import dimitri_dessus.labymotion.engines.GraphicGameEngine;
+import dimitri_dessus.labymotion.engines.PhysicalGameEngine;
+import dimitri_dessus.labymotion.models.Ball;
+import dimitri_dessus.labymotion.models.Bloc;
+
 public class GameActivity extends AppCompatActivity {
 
+    // Id of the victory dialog
+    public static final int VICTORY_DIALOG = 0;
+
+    // Id of the defeat dialog
+    public static final int DEFEAT_DIALOG = 1;
+
+    // Definition of GraphicEngine object
+    private GraphicGameEngine mView = null;
+    // Definition of PhysicalEngine object
+    private PhysicalGameEngine mEngine = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        mView = new GraphicGameEngine(this);
+        setContentView(mView);
+
+        mEngine = new PhysicalGameEngine(this);
+
+        Ball b = new Ball();
+        mView.setBall(b);
+        mEngine.setBall(b);
+
+        List<Bloc> mList = mEngine.buildLabyrinthe();
+        mView.setBlocks(mList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mEngine.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mEngine.stop();
+    }
+    
+    @Override
+    public Dialog onCreateDialog (int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        switch(id) {
+            case VICTORY_DIALOG:
+                builder.setCancelable(false)
+                        .setMessage(R.string.victory_title)
+                        .setTitle(R.string.victory_msg)
+                        .setNeutralButton(R.string.restart_game, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mEngine.reset();
+                                mEngine.resume();
+                            }
+                        });
+                break;
+
+            case DEFEAT_DIALOG:
+                builder.setCancelable(false)
+                        .setMessage(R.string.defeat_msg)
+                        .setTitle(R.string.defeat_title)
+                        .setNeutralButton(R.string.restart_game, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mEngine.reset();
+                                mEngine.resume();
+                            }
+                        });
+        }
+        return builder.create();
+    }
+
+    @Override
+    public void onPrepareDialog (int id, Dialog box) {
+        // When a dialog box is triggered, stop the game
+        mEngine.stop();
     }
 }
